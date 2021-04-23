30f3a7e5a4c331f3bef1d0d2b25ccad2af22fd26
Matt Wolford <matthew.r.wolford@gmail.com>
2021-04-23 01:29:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,8 +11,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.map do |student|
-      Student.new = student
-    end
+      Student.new(student)
   end
 
   end
